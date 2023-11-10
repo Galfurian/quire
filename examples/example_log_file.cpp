884700232dd22ebbd124d589410a0c9855feb69a
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2023-11-10 14:31:55 -0500

    Disable output stream in log on file example.

--- examples/example_log_file.cpp
+++ examples/example_log_file.cpp
@@ -4,12 +4,17 @@
 
 #include <quire/quire.hpp>
 
+#include <iostream>
+
 int main(int, char *[])
 {
+    char log_filename[] = "h0.log";
+
     auto l0 = std::make_shared<quire::logger_t>("L0", quire::log_level::debug, '|');
-    auto h0 = std::make_shared<quire::file_handler_t>("h0.log", "w");
+    auto h0 = std::make_shared<quire::file_handler_t>(log_filename, "w");
 
     l0->set_file_handler(h0);
+    l0->set_output_stream(nullptr);
 
     l0->set_header("L0");
     l0->set_log_level(quire::log_level::debug);
@@ -18,6 +23,8 @@ int main(int, char *[])
     l0->toggle_date(true);
     l0->toggle_time(true);
     l0->toggle_location(true);
+
+    std::cout << "We are saving the log to `" << log_filename << "`.\n";
 
     qdebug(l0, "Hello there, I'm logging on file!");
 
